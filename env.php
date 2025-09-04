67eee1b586b53cc154287bc5acdc41d01cc3b8b2
Adrian Humphrey <github@ember.co.nz>
2025-09-05 11:53:29 +1200

    updated dotenv syntax for v5

--- env.php
+++ env.php
@@ -7,11 +7,11 @@ if ($environment == 'development') {
     $dotenv->load();
 }
 
-$timezone                      = getenv('timezone');
-$total_working_hours_per_month = getenv('total_working_hours_per_month');
-$api_key                       = getenv('api_key');
-$api_version                   = getenv('api_version');
-$api_reporting_version         = getenv('api_reporting_version');
-$non_contact_days              = getenv('non_contact_days') ?: [];
-$additionally_skipped_days     = getenv('additionally_skipped_days') ?: [];
-$additional_working_days       = getenv('additional_working_days') ?: [];
+$timezone                      = $_ENV['timezone'];
+$total_working_hours_per_month = $_ENV['total_working_hours_per_month'];
+$api_key                       = $_ENV['api_key'];
+$api_version                   = $_ENV['api_version'];
+$api_reporting_version         = $_ENV['api_reporting_version'];
+$non_contact_days              = $_ENV['non_contact_days'] ?: [];
+$additionally_skipped_days     = $_ENV['additionally_skipped_days'] ?: [];
+$additional_working_days       = $_ENV['additional_working_days'] ?: [];
